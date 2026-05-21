a7748e6bbf22a81980568d7e3e29c63c909edd5b
utkarsha2496 <uswakodkar@gmail.com>
2026-05-21 16:20:11 +0530

    Initial commit

---
+++ src/main/java/com/ecart/digi_app/DigiAppApplication.java
@@ -0,0 +1,13 @@
+package com.ecart.digi_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecart/digi_app/DigiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecart.digi_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
